b1f48050c60d0a6a63fb4c32b2134b71ab373f10
akotek <kotekmath@gmail.com>
2020-08-23 15:53:51 +0300

    initial

---
+++ src/github_actions_test/core.clj
@@ -0,0 +1,22 @@
+(ns github-actions-test.core
+  (:require [clojure.java.jdbc :as j]))
+
+
+(def db
+  {:classname   "org.h2.Driver"
+   :subprotocol "h2:mem"
+   :subname     "demo;DB_CLOSE_DELAY=-1"
+   :user        "sa"
+   :password    ""})
+
+
+(j/execute! db["create table gl (id int primary key, age int, gender varchar(100), coverage int,
+monthly_ach double, monthly_cc double)"])
+;
+(j/execute! db ["insert into gl values
+  (1, 50, 'male', 5000, 127.7, 129.5),
+  (2, 50, 'male', 15000, 129.5, 130.5),
+  (3, 50, 'male', 25000, 130.5, 140.6),
+  (4, 50, 'female', 5000, 123,140)"])
+
+ (j/query db ["select * from gl"])
---
+++ test/github_actions_test/core_test.clj
@@ -0,0 +1,7 @@
+(ns github-actions-test.core-test
+  (:require [clojure.test :refer :all]
+            [github-actions-test.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
